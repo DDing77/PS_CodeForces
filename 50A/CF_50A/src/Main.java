2738c847add82488afb381f3b8b8ed3c41dc53ea
DDing77 <devseo2021@gmail.com>
2022-12-24 17:27:17 +0900

    CF_50A

---
+++ 50A/CF_50A/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int M, N;
+    static int res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        res = (M * N) / 2;
+
+        System.out.println(res);
+    }
+}
