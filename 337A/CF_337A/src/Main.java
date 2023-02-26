e480f517866d75605c49b52d75aeeb97a1fd611a
DDing77 <devseo2021@gmail.com>
2023-02-26 21:50:16 +0900

    CF_337A

---
+++ 337A/CF_337A/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static int res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        res = Integer.MAX_VALUE;
+        for (int i = 0; i < m - n + 1; i++) {
+            res = Math.min(res, arr[i + n - 1] - arr[i]);
+        }
+
+        System.out.println(res);
+    }
+}
