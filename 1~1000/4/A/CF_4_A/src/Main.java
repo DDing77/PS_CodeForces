5a946af31e42fb9f9e764c1450292917fcd2cd01
DDing77 <devseo2021@gmail.com>
2026-04-28 18:38:44 +0900

    CF_4_A

---
+++ 1~1000/4/A/CF_4_A/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+
+    private int w;
+
+    private void solution() throws IOException {
+        w = Integer.parseInt(br.readLine());
+
+
+        if (w >= 4 && (w & 1) != 1) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
