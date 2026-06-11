cde35640e15ce892ae09b7435e9b49ff6244e813
DDing77 <devseo2021@gmail.com>
2026-06-11 22:30:54 +0900

    CF_486_A

---
+++ 1~2000/486/src/A/Main.java
@@ -0,0 +1,30 @@
+package A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+
+    private long n;
+
+    private void solution() throws IOException {
+        n = Long.parseLong(br.readLine());
+
+        if (n % 2 == 1) {
+            n++;
+            n *= -1;
+            System.out.println(n / 2);
+        } else {
+            System.out.println(n / 2);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
