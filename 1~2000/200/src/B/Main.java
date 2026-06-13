6337e5f83bb7d19a7d7043a1686c4d0e97e9f8a3
DDing77 <devseo2021@gmail.com>
2026-06-13 23:52:56 +0900

    CF_200_B

---
+++ 1~2000/200/src/B/Main.java
@@ -0,0 +1,21 @@
+package B;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum / n);
+    }
+}
