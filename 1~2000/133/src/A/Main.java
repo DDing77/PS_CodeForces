4e034d81bed314a342b600401ac07c23394dc4cb
DDing77 <devseo2021@gmail.com>
2026-07-18 20:59:16 +0900

    CF_133_A

---
+++ 1~2000/133/src/A/Main.java
@@ -0,0 +1,28 @@
+package A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+
+    private void solution() throws IOException {
+        String input = br.readLine();
+
+        if (input.contains("H")
+                || input.contains("Q")
+                || input.contains("9")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
