ec1a5fb19563caec207cec40d3178aa3fa954e90
DDing77 <devseo2021@gmail.com>
2026-09-11 10:38:31 +0900

    CF_1807_A

---
+++ 1~2000/1807/src/A/Main.java
@@ -0,0 +1,41 @@
+package A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int t;
+    private int a;
+    private int b;
+    private int c;
+
+    private void solution() throws IOException {
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            if (a + b == c) {
+                sb.append("+");
+            } else {
+                sb.append("-");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
